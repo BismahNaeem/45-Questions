7a1c4396e8212c6c1026e3a9fb72b6969cb7250a
Bismah Naeem <163287902+BismahNaeem@users.noreply.github.com>
2024-03-21 19:54:47 +0500

    1st commit

---
+++ 7 ,8  Question plus,minus/app.ts
@@ -0,0 +1,22 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+// Be sure to enclose your operations in print statements to see the results.
+
+//You should create four lines that look like this:
+
+//console.log(5 + 3)
+
+//Your output should simply be four lines with the number 8 appearing once on each line.
+
+//addition
+console.log(4+4);
+
+//subtraction
+console.log(9-1);
+
+//multiplication
+console.log(4*2);
+
+//division
+console.log(16/2);
+
+
