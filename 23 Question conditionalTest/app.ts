2161b2352e29bf97677a97ae751e7acf125c78fb
Bismah Naeem <163287902+BismahNaeem@users.noreply.github.com>
2024-03-21 20:41:41 +0500

    1st commit

---
+++ 23 Question conditionalTest/app.ts
@@ -0,0 +1,50 @@
+//conditional Tests: Write a series of conditional tests. Print a statement describing each test and your 
+//prediction for the results of each test. Your code should look something like this:
+//let car = 'subaru';
+//console.log("Is car == 'subaru'? I predict True.")
+//console.log(car == 'subaru')
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+//true statemnets
+var car = `civic`;
+console.log("is car ==`civic`? i predict True.");
+console.log(car==`civic`)//true
+
+var car=`audi`;
+console.log("is car ==`audi`? i predict True.");
+console.log(car==`audi`)//true
+
+var car =`alto`;
+console.log("is car ==`alto`? i predict True.");
+console.log(car==`alto`)//true
+
+var car =`elantra`;
+console.log("is car ==`elantra`? i predict True.");
+console.log(car==`elantra`)//true
+
+var  car=`corolla`;
+console.log("is car ==`corolla`? i predict True.");
+console.log(car==`corolla`)//true
+
+//false statements
+var book =`typescript`;
+console.log("is book !==typescript ?i predict false ")
+console.log(book!==`typescript`)//false
+
+var book =`english`;
+console.log("is book !==typescript ?i predict false ")
+console.log(book!==`english`)//false
+
+var book =`urdu`;
+console.log("is book !==typescript ?i predict false ")
+console.log(book!==`urdu`)//false
+
+
+var book =`math`;
+console.log("is book !==typescript ?i predict false ")
+console.log(book!==`math`)//false
+
+var book =`typescript`;
+console.log("is book !==typescript ?i predict false ")
+console.log(book!==`typescript`)//false
