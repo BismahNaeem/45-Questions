dde7c6c0d060de82886f71760326770e79ded430
Bismah Naeem <163287902+BismahNaeem@users.noreply.github.com>
2024-03-21 19:52:29 +0500

    1st commit

---
+++ 6 question  addingWhitespace/app.js
@@ -0,0 +1,10 @@
+"use strict";
+//stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name.
+//Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, 
+//so the whitespace around the name is displayed.
+//then print the name after striping the white spaces.
+//with whitespace
+let herName = "\n\t naeem\t\n";
+console.log(herName);
+//without whitespace
+console.log(herName.trim());
---
+++ 6 question  addingWhitespace/app.ts
@@ -0,0 +1,12 @@
+//stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name.
+ //Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, 
+ //so the whitespace around the name is displayed.
+ //then print the name after striping the white spaces.
+
+//with whitespace
+
+ let herName:string = "\n\t naeem\t\n";
+ console.log(herName);
+
+ //without whitespace
+ console.log(herName.trim())
