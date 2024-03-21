e2df13ea57275af0c6a4385af9f43e2f77f3cf93
Bismah Naeem <163287902+BismahNaeem@users.noreply.github.com>
2024-03-21 19:55:58 +0500

    1st commit

---
+++ 9 Question favouriteNumber/app.js
@@ -0,0 +1,6 @@
+"use strict";
+//favorite Number: Store your favorite number in a variable. Then, using that variable,
+//create a message that reveals your favorite number. Print that message.
+let favoriteNumber = "15";
+let messege = `my favourite number is: ${favoriteNumber}`;
+console.log(messege);
---
+++ 9 Question favouriteNumber/app.ts
@@ -0,0 +1,8 @@
+//favorite Number: Store your favorite number in a variable. Then, using that variable,
+ //create a message that reveals your favorite number. Print that message.
+
+ let favoriteNumber:string ="15"
+
+ let messege: string=`my favourite number is: ${favoriteNumber}`;
+
+ console.log(messege);
