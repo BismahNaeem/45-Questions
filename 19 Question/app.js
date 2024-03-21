219f5b89c1b24ae47f2fe6fe2c23d8978888db6b
Bismah Naeem <163287902+BismahNaeem@users.noreply.github.com>
2024-03-21 20:19:27 +0500

    1st commit

---
+++ 19 Question/app.js
@@ -0,0 +1,3 @@
+//question# 19
+var guests = ["Ayesha", "ulfat", "anabia", "saba", "hiba"];
+console.log("I am inviting ".concat(guests.length, " guests to dinner."));
---
+++ 19 Question/app.ts
@@ -0,0 +1,5 @@
+//question# 19
+
+let guests: string[] = ["Ayesha", "ulfat", "anabia", "saba", "hiba"];
+
+console.log(`I am inviting ${guests.length} guests to dinner.`);
