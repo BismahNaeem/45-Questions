dabdc9427c457aa9e0b7e783fe0bb2ee6e95e1ca
Bismah Naeem <163287902+BismahNaeem@users.noreply.github.com>
2024-03-21 20:36:59 +0500

    1st commit

---
+++ 22  Question CreatingError/app.js
@@ -0,0 +1,9 @@
+"use strict";
+//Intentional Error: If you haven’t received an array index error in one of your programs yet,
+// try to make one happen. Change an index in one of your programs to produce an index error.
+// Make sure you correct the error before closing the program.
+let cars = ["audi", "civic", "landcruosier",];
+//let put no 3 to execute an error becoz no 3 is not in the list
+console.log(cars[3]);
+//correcting the error by putting  no who is in  the list
+console.log(cars[0]);
---
+++ 22  Question CreatingError/app.ts
@@ -0,0 +1,11 @@
+//Intentional Error: If you haven’t received an array index error in one of your programs yet,
+// try to make one happen. Change an index in one of your programs to produce an index error.
+// Make sure you correct the error before closing the program.
+
+let cars: string [] =["audi", "civic" ,"landcruosier",]
+
+//let put no 3 to execute an error becoz no 3 is not in the list
+console.log(cars[3])
+
+//correcting the error by putting the  no who is in  the list
+console.log(cars[0])
