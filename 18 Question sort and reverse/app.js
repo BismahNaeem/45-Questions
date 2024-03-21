62b426c60f76030c422bc9788770076ee0bdf661
Bismah Naeem <163287902+BismahNaeem@users.noreply.github.com>
2024-03-21 20:12:32 +0500

    1st commit

---
+++ 18 Question sort and reverse/app.js
@@ -0,0 +1,19 @@
+"use strict";
+//create an  array in which we store a favourite places
+let favouritePlaces = ["saudiaArab", "london ", "karachi", "lahore", "islamabad"];
+//print this array
+console.log(favouritePlaces);
+//print the array in alphabetical order
+console.log([...favouritePlaces].sort());
+//print the array in its original order
+console.log(favouritePlaces);
+//print your array in reverse alphabetical order
+console.log([...favouritePlaces].sort().reverse());
+//again print the array in its original form
+console.log(favouritePlaces);
+//again reverse the order of list
+console.log([...favouritePlaces].sort().reverse());
+//again print the array in its original form
+console.log(favouritePlaces);
+//again print the array in alphabetical order
+console.log([...favouritePlaces].sort());
---
+++ 18 Question sort and reverse/app.ts
@@ -0,0 +1,32 @@
+//quesion no#18
+
+//create an  array in which we store a favourite places
+let favouritePlaces: string[] =["saudiaArab","london ","karachi","lahore","islamabad"];
+
+//print this array
+console.log(favouritePlaces);
+
+//print the array in alphabetical order
+console.log([...favouritePlaces].sort());
+
+//print the array in its original order
+console.log(favouritePlaces)
+
+//print your array in reverse order
+console.log([...favouritePlaces].sort().reverse());
+
+//again print the array in its original form
+console.log(favouritePlaces);
+
+//again reverse the order of list
+console.log([...favouritePlaces].sort().reverse());
+
+//again print the array in its original form
+console.log(favouritePlaces);
+
+//again print the array in alphabetical order
+console.log([...favouritePlaces].sort());
+
+
+
+
