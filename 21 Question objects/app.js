9876f96e0e22bb23d479ae5e3cc25c521e5f46d5
Bismah Naeem <163287902+BismahNaeem@users.noreply.github.com>
2024-03-21 20:24:15 +0500

    1st commit

---
+++ 21 Question objects/app.js
@@ -0,0 +1,9 @@
+"use strict";
+//They think of something you could store in a TypeScript Object.
+//write a program that creates Objects containing these items.
+let book = {
+    name: "typscript",
+    price: 200,
+    author: "unknown",
+};
+console.log(`book info:${book.name} by ${book.author} and its price is  ${book.price}`);
---
+++ 21 Question objects/app.ts
@@ -0,0 +1,15 @@
+//They think of something you could store in a TypeScript Object.
+ //write a program that creates Objects containing these items.
+
+ let book: {name:string ;
+          price:number;
+          author:string
+}=
+ {
+    name : "typscript",
+    price :  200,
+    author : "unknown",
+ }
+
+ console.log(`book info:${book.name} by ${book.author} and its price is  ${book.price}`)
+
