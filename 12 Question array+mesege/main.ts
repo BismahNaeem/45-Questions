d4908184e4a92b22503b7e80282831f569e640d3
Bismah Naeem <163287902+BismahNaeem@users.noreply.github.com>
2024-03-21 20:01:19 +0500

    1st commit

---
+++ 12 Question array+mesege/main.ts
@@ -0,0 +1,9 @@
+//Greetings: Start with the array you used in Exercise 11,
+ //but instead of just printing each person’s name, print a message to them.
+ // The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names: string[] =["aisha", "aizal","arina", "aiza", "ambreen"];
+let messege="helo its me bia and i want to tell you something new about coding are you excited; ";
+for (let i=0;i< names. length; i++)
+{console.log(messege +names[i]);}
+
